837d54d99bd6241a7d45791e005cfb7a92d3e912
Renzel Belgado <30442976+darkrenz@users.noreply.github.com>
2021-08-17 16:19:48 +0800

    Update ssh-only.sh

--- ssh-only.sh
+++ ssh-only.sh
@@ -95,7 +95,7 @@ apt -y install nginx
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Alamyazid/Autoscript/main/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/darkrenz/myscript1/main/nginx.conf"
 mkdir -p /home/vps/public_html
 echo "<pre>Mod By TuanYz</pre>" > /home/vps/public_html/index.html
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Alamyazid/Autoscript/main/vps.conf"
