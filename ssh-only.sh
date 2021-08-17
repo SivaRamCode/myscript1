6201432e1636df0aaa532155db57f5c8a7257963
Renzel Belgado <30442976+darkrenz@users.noreply.github.com>
2021-08-17 21:31:15 +0800

    Update ssh-only.sh

--- ssh-only.sh
+++ ssh-only.sh
@@ -235,11 +235,7 @@ echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 cd
 apt install -y libxml-parser-perl
 
-
-# Custom Banner SSH
-
-
-echo "================  Banner ======================"
+# banner /etc/issue.net
 wget -O /etc/issue.net "https://raw.githubusercontent.com/darkrenz/myscript1/main/bannerssh.conf"
 echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
