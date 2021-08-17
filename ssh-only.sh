658703eca14dade4b1c7769d878cf0572c27652c
Renzel Belgado <30442976+darkrenz@users.noreply.github.com>
2021-08-17 23:41:32 +0800

    Update ssh-only.sh

--- ssh-only.sh
+++ ssh-only.sh
@@ -282,7 +282,6 @@ wget -O autokill "https://raw.githubusercontent.com/darkrenz/myscript1/main/auto
 wget -O ceklim "https://raw.githubusercontent.com/darkrenz/myscript1/main/ceklim.sh"
 wget -O tendang "https://raw.githubusercontent.com/darkrenz/myscript1/main/tendang.sh"
 wget -O clear-log "https://raw.githubusercontent.com/darkrenz/myscript1/main/clear-log.sh"
-wget -O bannerssh "https:''raw.githubusercontent.con/darkrenz/myscrit1/main/bannerssh.conf"
 
 echo "0 5 * * * root clear-log && reboot" >> /etc/crontab
 
