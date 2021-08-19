5d074e362c44e976abbd22ae5bf5f4274c4e97e8
Renzel Belgado <30442976+darkrenz@users.noreply.github.com>
2021-08-19 23:50:18 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -241,8 +241,8 @@ echo "Banner /etc/issue.net" >>/etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 
 #install bbr dan optimasi kernel
-wget https://raw.githubusercontent.com/darkrenz/myscript1/main/bbr.sh && chmod +x bbr.sh && ./bbr.sh
-wget https://raw.githubusercontent.com/darkrenz/myscript1/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+#wget https://raw.githubusercontent.com/darkrenz/myscript1/main/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+#wget https://raw.githubusercontent.com/darkrenz/myscript1/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 
 # blockir torrent
 iptables -A FORWARD -m string --string "get_peers" --algo bm -j DROP
